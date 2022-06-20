ff4dc14aef81b5b13691e8aef81cb63fcb283cf6
Usman Sharafa <103945182+Usmansharafa@users.noreply.github.com>
2022-06-20 21:36:10 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,26 @@
+var expression = prompt("Enter an expression");
+var x,y,z;
+var answer;
+[x,y,z] = expression.split(" ");
+x = Number(x);
+z = Number(z);
+switch(y){
+   case "+":
+        answer = x + z;
+        console.log(answer);
+        break;
+   case "-":
+        answer = x - z;
+        console.log(answer);
+        break;
+   case "*":
+        answer = x * z;
+        console.log(answer);
+        break;
+   case "/":
+        answer = x / z;
+        console.log(answer);
+        break;
+   default:
+        console.log("Operation cannot be performed")
+}

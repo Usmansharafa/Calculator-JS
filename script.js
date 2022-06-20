eaa16e63b90898cb9020b5d6d867c06bfb0a7528
Usman Sharafa <103945182+Usmansharafa@users.noreply.github.com>
2022-06-20 22:08:38 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,25 @@
-//Give a space between your expression
-var expression = prompt("Enter an expression");
-var x,y,z;
-var answer;
-[x,y,z] = expression.split(" ");
+var x = prompt("Enter First Number");
+var z = prompt("Enter Second Number");
+var y = prompt("Enter an Operator like '+','-','*','/'");
 x = Number(x);
 z = Number(z);
 switch(y){
-   case "+":
-        answer = x + z;
-        console.log(answer);
-        break;
-   case "-":
-        answer = x - z;
-        console.log(answer);
-        break;
-   case "*":
-        answer = x * z;
-        console.log(answer);
-        break;
-   case "/":
-        answer = x / z;
-        console.log(answer);
-        break;
-   default:
-        console.log("Operation cannot be performed")
+  case "+":
+    answer = x + z;
+    alert(answer);
+    break;
+  case "-":
+    answer = x - z;
+    alert(answer);
+    break;
+  case "/":
+    answer = x / z;
+    alert(answer);
+    break;
+  case "*":
+    answer = x * z;
+    alert(answer);
+    break;
+  default:
+    alert("Operation cannot be performed")
 }
